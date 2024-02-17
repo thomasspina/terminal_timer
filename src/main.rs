0d006ca6beea3ebfe83b35a42f7032400701ef08
Thomas Spina <thomas@thomasspina.com>
2024-02-17 15:52:27 -0500

    finished the display of the timer. looks good i think

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::{
     io::{stdout, Write},
     thread::sleep,
     time::{Duration, SystemTime},
-    //env,
 };
+use crossterm::{QueueableCommand, cursor};
 
 struct TimeUnits {
     h: u8,
@@ -20,15 +20,29 @@ fn get_time_units(s: &u64)->TimeUnits {
 }
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
     
     let now = SystemTime::now();
     let mut stdout = stdout();
+
+    _ = stdout.queue(cursor::SavePosition);
+
     loop {
         match now.elapsed() {
             Ok(elapsed) => {
                 let time_units: TimeUnits = get_time_units(&elapsed.as_secs());
-                print!("\r{}h{}m{}s", time_units.h, time_units.m, time_units.s);
+                _ = stdout.queue(cursor::RestorePosition);
+                
+                // get width of terminal window in columns
+                if let Some((w, _)) = term_size::dimensions() {
+                    print!("\r{}", " ".repeat(w)); // wipe clean the current line
+                }
+
+                let h_0: char = if time_units.h < 10 {'0'} else {0 as char};
+                let s_0: char = if time_units.s < 10 {'0'} else {0 as char};
+                let m_0: char = if time_units.m < 10 {'0'} else {0 as char};
+
+                print!("\r{}{}:{}{}:{}{}", 
+                        h_0, time_units.h, m_0, time_units.m, s_0, time_units.s);
                 stdout.flush().unwrap();
             }
             Err(e) => {
