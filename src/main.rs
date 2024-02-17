163b6adf5383cf2df3efd2773b94fa2a90f3e4fc
Thomas Spina <thomas@thomasspina.com>
2024-02-17 17:37:35 -0500

    added keystroke bindings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::{
-    io::{stdout, Write},
-    thread::sleep,
+    io::{self, stdout, Write},
     time::{Duration, SystemTime},
 };
-use crossterm::{QueueableCommand, cursor};
+use crossterm::{
+    cursor, event::{poll, read, Event, KeyCode}, QueueableCommand
+};
 
 struct TimeUnits {
     h: u8,
@@ -11,7 +12,7 @@ struct TimeUnits {
     s: u8,
 }
 
-fn get_time_units(s: &u64)->TimeUnits {
+fn get_time_units(s: &u64) -> TimeUnits {
     TimeUnits {
         h: (s / 3600) as u8,
         m: ((s - ((s / 3600) * 3600)) / 60) as u8,
@@ -19,24 +20,29 @@ fn get_time_units(s: &u64)->TimeUnits {
     }
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     
     let now = SystemTime::now();
     let mut stdout = stdout();
 
-    _ = stdout.queue(cursor::SavePosition);
-
+    stdout.queue(cursor::SavePosition)?;
     loop {
         match now.elapsed() {
             Ok(elapsed) => {
                 let time_units: TimeUnits = get_time_units(&elapsed.as_secs());
-                _ = stdout.queue(cursor::RestorePosition);
+                stdout.queue(cursor::RestorePosition)?;
                 
                 // get width of terminal window in columns
-                if let Some((w, _)) = term_size::dimensions() {
-                    print!("\r{}", " ".repeat(w)); // wipe clean the current line
+                if let Some((w, h)) = term_size::dimensions() {
+                    // for loop in ordered to wipe clean every line
+                    for _ in 1..(h - cursor::position().unwrap().1 as usize) {
+                        print!("{}\n", " ".repeat(w)); // wipe clean the line
+                    }
                 }
 
+                stdout.queue(cursor::RestorePosition)?;
+                
+                // add a zero in front if below ten
                 let h_0: char = if time_units.h < 10 {'0'} else {0 as char};
                 let s_0: char = if time_units.s < 10 {'0'} else {0 as char};
                 let m_0: char = if time_units.m < 10 {'0'} else {0 as char};
@@ -49,6 +55,17 @@ fn main() {
                 println!("Error: {e:?}");
             }
         }
-        sleep(Duration::new(1, 0));
+
+        // check for key presses
+        if poll(Duration::from_millis(1_000))? {
+            let event = read()?;
+            if event == Event::Key(KeyCode::Char('p').into()) {
+                // TODO add pause when p is pressed or spacebar
+                // TODO add start back up when spacebar or p
+            }
+
+        } else {
+            println!(".\r");
+        }
     }
 }
